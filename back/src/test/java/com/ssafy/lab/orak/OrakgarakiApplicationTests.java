79fb4c28c97dde592c7c98452b59ba120595faec
범수 <dj258255@naver.com>
2025-09-08 15:44:53 +0900

    chore:SpringBoot 초기 세팅

---
+++ back/src/test/java/com/ssafy/lab/orak/OrakgarakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.lab.orak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrakgarakiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
