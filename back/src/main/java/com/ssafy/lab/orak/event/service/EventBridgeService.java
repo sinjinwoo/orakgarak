57f08fbba50eb2a51a77b25bd7e9480bb64c5f64
범수 <dj258255@naver.com>
2025-09-18 16:18:21 +0900

    fix : 오류수정

--- back/src/main/java/com/ssafy/lab/orak/event/service/EventBridgeService.java
+++ back/src/main/java/com/ssafy/lab/orak/event/service/EventBridgeService.java
@@ -8,7 +8,6 @@ import com.ssafy.lab.orak.event.exception.KafkaSendException;
 import io.micrometer.core.instrument.Counter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
