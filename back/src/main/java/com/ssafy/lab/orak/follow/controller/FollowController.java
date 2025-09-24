3ade7c1481167162385360d2ba05046d34ddbd5c
이혜민 <hy1x1mn@gmail.com>
2025-09-24 14:58:33 +0900

    fix: social 매핑 주소 수정

--- back/src/main/java/com/ssafy/lab/orak/follow/controller/FollowController.java
+++ back/src/main/java/com/ssafy/lab/orak/follow/controller/FollowController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/social")
+@RequestMapping("/social")
 @RequiredArgsConstructor
 public class FollowController {
 
