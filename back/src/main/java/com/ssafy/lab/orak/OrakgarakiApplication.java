1fafa139d3264c7e6fefb20ac751a0c88e668ec7
범수 <dj258255@naver.com>
2025-09-27 00:58:50 +0900

    feat : 오류 제거

--- back/src/main/java/com/ssafy/lab/orak/OrakgarakiApplication.java
+++ back/src/main/java/com/ssafy/lab/orak/OrakgarakiApplication.java
@@ -3,8 +3,10 @@ package com.ssafy.lab.orak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableJpaAuditing
+@EnableKafka
 @SpringBootApplication
 public class OrakgarakiApplication {
 
